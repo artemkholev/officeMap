57b254c034d825af090b41d2e5774bfb2fa4d2a1
Artem <artemkholev@yandex.ru>
2024-01-22 11:47:52 +0300

    add input

--- frontend/src/shered/UI/index.ts
+++ frontend/src/shered/UI/index.ts
@@ -4,5 +4,6 @@ import SidebarElem from "./SidebarElem/SidebarElem.vue";
 import SelectElem from "./SelectElem/SelectElem.vue";
 import UBreadcrump from "./UBreadcrumb/UBreadcrumb.vue";
 import DialogWindow from "./DialogWindow/DialogWindow.vue";
+import InputElem from "./InputElem/InputElem.vue"
 
-export default [ButtonElem, BurgerElem, SidebarElem, SelectElem, UBreadcrump, DialogWindow];
+export default [ButtonElem, BurgerElem, SidebarElem, SelectElem, UBreadcrump, DialogWindow, InputElem];
